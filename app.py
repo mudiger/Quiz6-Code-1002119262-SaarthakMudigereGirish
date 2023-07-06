70c52d25cd4e47de470f8a54de7498764112980d
Noobalert <saarthak309@gmail.com>
2023-07-06 16:44:59 -0500

    test-2

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 rooms = {}
+question = ""
+answer = ""
 
-#shru
 class Game:
     def __init__(self):
         self.piles = []
@@ -13,15 +14,29 @@ class Game:
         self.min_stones = None
         self.max_stones = None
         self.cumulative_scores = {}  # Dictionary to store cumulative scores
+        self.question = None
+        self.answer = None
+        self.answer1 = None
+        self.answer2 = None
 
     def add_player(self, player_name):
         self.players.append(player_name)
         self.cumulative_scores[player_name] = 0  # Initialize cumulative score for the player
 
-    def judge_values(self, min_stones, max_stones, x, y, z):
-        self.min_stones = min_stones
-        self.max_stones = max_stones
-        self.piles = [x, y, z]
+    def judge_values(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+    def check_answer_class(self, answer1):
+        self.answer1 = answer1
+        self.current_player = self.players[0]
+        if self.answer == self.answer1:
+            print("OK")
+            self.cumulative_scores[self.current_player] += 1
+        else:
+            print("NO")
+            self.cumulative_scores[self.current_player] -= 2
+        self.current_player = self.get_next_player()
 
     def start_game(self):
         self.current_player = self.players[0]
@@ -66,25 +81,27 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/pick_stones', methods=['POST'])
-def pick_stones():
-    pile_index = int(request.form['pile_index'])
-    num_stones = int(request.form['num_stones'])
+@app.route('/check_answer1', methods=['POST'])
+def check_answer1():
+    answer1 = request.form['answer1']
+    game.check_answer_class(answer1)
+    return render_template('player1.html', game=game)
 
-    error_message = game.pick_stones(pile_index, num_stones)
 
-    return render_template('player1.html', game=game, error_message=error_message)
+@app.route('/check_answer2', methods=['POST'])
+def check_answer2():
+    answer2 = request.form['answer2']
+    game.check_answer_class(answer2)
+    return render_template('player2.html', game=game)
 
 
 @app.route("/judge", methods=["POST", "GET"])
 def judge():
+    global question
     if request.method == "POST":
-        min_stones = int(request.form['min'])
-        max_stones = int(request.form['max'])
-        x = int(request.form['x'])
-        y = int(request.form['y'])
-        z = int(request.form['z'])
-        game.judge_values(min_stones, max_stones, x, y, z)
+        question = request.form['question']
+        answer = request.form['answer']
+        game.judge_values(question, answer)
     return render_template("judge.html", game=game)
 
 
@@ -94,18 +111,39 @@ def player1():
     if request.method == "POST":
         player1 = request.form['player1']
         game.add_player(player1)
-        game.start_game()
-    return render_template("player1.html", game=game, player1=player1)
+        #game.start_game()
+    return render_template("player1.html", game=game, player1=player1, question = question)
 
 
 @app.route("/player2", methods=["POST", "GET"])
 def player2():
+    player2 = ""
     if request.method == "POST":
         player2 = request.form['player2']
         game.add_player(player2)
-        game.start_game()
-    return render_template("player2.html", game=game)
+        #game.start_game()
+    return render_template("player2.html", game=game, player2=player2, question = question)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# A History Bee (HB) is a two player game with an additional person as the judge. We will call those P1, P2, and J.
+#   The J gives ("sends") each player a question, such as "When was the war of 1812?" P1 and P2 try to respond as quickly as possible,
+#   The J sees their responses (if any) and responds to each in time order (first one submitted, if correct, gets points, if not correct,
+#   looks at the other player's submission, continue on for a maximum of 4 attempts, or as soon as one is correct.) The J gives feedback
+#   to each responce (OK, NO), Each player gives their name at the start, and each player sees both names and the scores.
+#   Each player starts with N points (set by J), and a correct answer score +1 point, an incorrect answer -2 points)
+#   To ensure that the game is fair, all questions and responses are logged in a text window that only the J sees,
+#   each line (entry) in the format:
+#   [time, originator] message text
+#   for example:
+#   [0, Joe] Where is the Alamo?
+#   [12, Pat] In Dallas
+#   [14, Judge] NO
+#   [50, Penny] San Antonio
+#   [51, Judge] OK
+#   The time may either be in seconds since the game began, or the actual system time if you prefer. On the J screen show current time
+#   in either format.
+#   The J at anytime may restart the game.
